cee436c1f12de746a9f8444a1f617f8d4c1c415e
Eric Wong <imericlwong@gmail.com>
2023-04-05 12:57:54 -0700

    Call add function if operator is add

--- calculator.py
+++ calculator.py
@@ -4,4 +4,28 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+while True:
+    user_input = input("> ")
+
+    tokens = user_input.split(" ")
+
+    if tokens[0] == "q":
+        print("Exiting calculator.")
+        break
+
+    operator = tokens[0]
+    num1 = tokens[1]
+
+    if len(tokens) == 3:
+        num2 = tokens[2]
+    elif len(tokens) == 4:
+        num3 = tokens[3]
+
+    result = None
+
+    if operator == "+":
+        result = add(float(num1), float(num2))
+    elif operator == "-":
+        result = subtract(float(num1), float(num2))
+    
+    print(result)
