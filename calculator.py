245948a6012d1e88afead04cf2b5fd27ea04af85
Eric Wong <imericlwong@gmail.com>
2023-04-05 13:59:42 -0700

    Fix input check and add comments

--- calculator.py
+++ calculator.py
@@ -11,20 +11,30 @@ while True:
     if tokens[0] == "q":
         print("Exiting calculator.")
         break
+    elif len(tokens) < 2:
+        print("Please provide enough inputs.")
+        continue
 
     operator = tokens[0]
     num1 = tokens[1]
 
+    # If there are three tokens, then the second number should be the last token
     if len(tokens) == 3:
         num2 = tokens[2]
 
+    # Try except block here to handle scenario where user inputs too few numbers for a given operator
     try:
+        # Check to make sure numbers inputted are valid numeric strings
         if not num1.isdigit() or not num2.isdigit():
             print("Please enter numbers after the operator.")
             continue
 
+        # We need a variable to store the result returned from each math function
         result = None
 
+        # Check each operator
+        # For each math function call, we need to cast each number string to a float
+        # in order to return results of type float
         if operator == "+":
             result = add(float(num1), float(num2))
         elif operator == "-":
@@ -45,6 +55,6 @@ while True:
             print("Please input a valid operator.")
 
         print(result)
-        
+
     except NameError:
         print("Please provide the appropriate length of numbers for each operator.")
